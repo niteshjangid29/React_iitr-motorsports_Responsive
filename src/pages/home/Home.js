92379320a3bd341895079856c729100b7bef3908
Nitesh Jangid <99542740+niteshjangid29@users.noreply.github.com>
2022-05-24 11:28:57 +0530

    new

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './Home.css'
 import Navbars from '../../components/Navbar/Navbar'
 import Slider from '../../components/Slider/Slider'
